0fc4bc7083fe32004f6d1de3f7f9c82f5387069b
nesterenkosu <62102496+nesterenkosu@users.noreply.github.com>
2023-10-06 17:48:14 +0500

    Add files via upload

---
+++ DOTNET/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookComputing.XmlRpc;
+
+namespace XmlRpcClient2023Console
+{
+    [XmlRpcUrl("https://localhost:44302/Server.ashx")]
+    public interface IDemoServiceProxy : IXmlRpcProxy
+    {
+        [XmlRpcMethod("demoservice::hellouser")]
+        string HelloUser(string name, int age);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDemoServiceProxy proxy = XmlRpcProxyGen.Create<IDemoServiceProxy>();
+            try
+            {
+                string response = proxy.HelloUser("Andrew", 2);
+                Console.WriteLine(response);
+            }
+            catch(System.Net.WebException ex)
+            {
+                //Ошибка при передаче запроса
+            }
+            catch(XmlRpcServerException ex)
+            {
+                //Ошибка сервера
+            }
+            catch (XmlRpcFaultException ex)
+            {
+                //Штатная ошибка метода
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DOTNET/Server.ashx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CookComputing.XmlRpc;
+
+namespace XmlRpcServer2023
+{
+    /// <summary>
+    /// Сводное описание для Server
+    /// </summary>
+    public class Server : XmlRpcService
+    {
+       [XmlRpcMethod("demoservice::hellouser")]
+       public string HelloUser(string name, int age)
+       {
+            if (age < 18)
+                throw new XmlRpcFaultException(1, "Детям нельзя!");
+            
+            return "Добро пожаловать, " + name + "!";
+       }
+    }
+}
